5912f122713426cb63b53ecead2853477cd0a978
Julian Hernandez <hernandezjuliang44@gmail.com>
2024-05-20 11:25:34 -0600

    Mod Tab and Malachite
    Custom Creative Mode Tab added
    Temporary item "Malachite" added

---
+++ src/main/java/net/elreyjota/revenantmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.elreyjota.revenantmod.item;
+
+import net.elreyjota.revenantmod.RevenantMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, RevenantMod.MOD_ID);
+
+    public static final RegistryObject<Item> MALACHITE = ITEMS.register("malachite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LOGO = ITEMS.register("logo",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
