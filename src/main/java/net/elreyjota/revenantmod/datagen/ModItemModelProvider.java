927e9e84ffd86852f11f6623b7464c75d1547fe4
Julian Hernandez <hernandezjuliang44@gmail.com>
2024-05-24 11:34:54 -0600

    Data gen changes
    -Added data gen json files
    -Delete relevant non-data gen json files

--- src/main/java/net/elreyjota/revenantmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/elreyjota/revenantmod/datagen/ModItemModelProvider.java
@@ -24,6 +24,7 @@ public class ModItemModelProvider extends ItemModelProvider {
         simpleItem(ModItems.WOOD_HANDLE);
         simpleItem(ModItems.PLASMA_CRUDE);
         simpleItem(ModItems.PYRIUM_RAW);
+        simpleItem(ModItems.LOGO);
     }
 
     // The Item Model Builder out of an Item registry
